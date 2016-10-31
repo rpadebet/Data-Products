fa3afc65c53820fd2d8748e468eea3de1c8f85b4
rpadebet <rohit.pittu@gmail.com>
2016-10-30 22:26:44 -0400

    Added Documentation to the app

--- Final Project/Portfolio Benchmark/ui.R
+++ Final Project/Portfolio Benchmark/ui.R
@@ -12,10 +12,14 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("Monkey Capital Management LLC"),
   h2("Can a Monkey beat the S&P 500?"),
-  p("The Portfolio Manager here randomly picks an assigned number of stocks and
-    tries hard to beat the S&P 500 Total Return Index. Your job is to let him 
-    know how hard he can try and for how long he can play"),
-  p(strong("The nice thing about this manager: He doesn't charge you for extra work!")),
+  p("The Portfolio Manager here picks an assigned number of stocks once
+    and never thinks about it again, as he uses a highly sophisticated 
+    RANDOM stock picking strategy.(For now he is limited to the Dow 30 names given his IQ)"),
+  p("He wants to show you he is working though, so he tries hard to beat the 
+    S&P 500 Total Return Index every day."),
+  p("Your job is to let him know how hard he can work and for how long he can play"),
+  p(strong("The nicest thing about this manager: He doesn't charge you for extra work!")),
+  p("He also makes his TOP STOCK PICKS and Performance data readily available for all to see"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
@@ -32,7 +36,7 @@ shinyUI(fluidPage(
                   value = 5),
       radioButtons("rebal",
                    "How hard do you want the Monkey to work?",
-                   c("Buy and Hold (Lazy)" = "none",
+                   c(
                      "Rebalances Daily" = "days",
                      "Rebalances Weekly" = "weeks",
                      "Rebalances Monthly" = "months",
